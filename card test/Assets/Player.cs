577f06ed0cde632d2e612e19e9a1299a845b0a45
yehyune0201 <yehyune0201@gmail.com>
2026-05-26 16:11:14 +0900

    유니티 챤과 키네마틱 캐릭터 컨트롤러

---
+++ card test/Assets/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.W))
+        //{
+        //    transform.position += Vector3.fwd * speed * Time.deltaTime;
+        //}
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    transform.position += Vector3.back * speed * Time.deltaTime;
+        //}
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    transform.position += Vector3.right * speed * Time.deltaTime;
+        //}
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    transform.position += Vector3.left * speed * Time.deltaTime;
+        //}
+    }
+}
